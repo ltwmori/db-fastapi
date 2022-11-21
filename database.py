bc597d6fdf9dd13e9c227198cba8b89db3097068
ltwmori <ltwmori@gmail.com>
2022-11-21 15:36:47 +0600

    new

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://pjyqaxfsuxclil:b022a8e56f9fc6bb26cdea8ece92cedcc7fdb5696f9fbeb2cab81632565b6900@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/d8ps1mfph35nnh", echo=True)
+final_db_url ="postgresql://postgres:l8E8bq3zP2IDXejF@db.tymojvhecoudapuzesbu.supabase.co:5432/postgres"
+
+engine = create_engine(final_db_url, echo=True)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
